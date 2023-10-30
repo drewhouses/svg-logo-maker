10866f3e74aa673d3dce25f36396ab4f6882f50f
drewhouses <drewcasas@live.com>
2023-10-29 19:58:48 -0500

    finished main functionality, started jest testing

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
 const inquirer = require("inquirer");
-const Shape = require("./lib/shapes.js");
 const Triangle = require("./lib/triangle.js");
 const Square = require("./lib/square.js");
 const Circle = require("./lib/circle.js");
+const fs = require("fs");
 
-const questions = [
-  "Enter up to 3 characters for your logo: ",
-  "What color text would you like?",
-  "Which shape would you like to use?",
-  "What color do you want the shape to be?",
-];
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("logo generated")
+  );
+}
 
 function init() {
   inquirer
     .prompt([
       {
         type: "input",
-        message: questions[0],
+        message: "Enter up to 3 characters for your logo: ",
         name: "letters",
+        validate: function (input) {
+          if (input.length > 3) {
+            return "Must be no greather than 3 characters";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "input",
-        message: questions[1],
+        message: "What color text would you like?",
         name: "textColor",
       },
       {
         type: "list",
-        message: questions[2],
+        message: "Which shape would you like to use?",
         name: "shape",
         choices: ["Circle", "Triangle", "Square"],
       },
       {
         type: "input",
-        message: questions[3],
+        message: "What color do you want the shape to be?",
         name: "shapeColor",
       },
     ])
     .then((response) => {
+      console.log(response.shape);
+      let shape;
       switch (response.shape) {
-        case "circle":
+        case "Circle":
+          shape = new Circle(
+            response.shapeColor,
+            response.textColor,
+            response.letters
+          );
           break;
-        case "square":
+        case "Square":
+          shape = new Square(
+            response.shapeColor,
+            response.textColor,
+            response.letters
+          );
           break;
-        case "triangle":
+        case "Triangle":
+          shape = new Triangle(
+            response.shapeColor,
+            response.textColor,
+            response.letters
+          );
           break;
       }
+      writeToFile(`logo.svg`, shape.render());
     });
 }
 
